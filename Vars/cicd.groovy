f3826bd0c373c411f5482effed40286b63cd66a5
baigasif42 <148427844+baigasif42@users.noreply.github.com>
2023-11-14 10:37:29 +0530

    Create cicd.groovy

---
+++ Vars/cicd.groovy
@@ -0,0 +1,20 @@
+def newDownload(repo)
+{
+  git "https://github.com/intelliqittrainings/ ${repo}
+}
+def newBuild()
+{
+  sh 'mvn package'
+}
+def newDeploy(Jobname,IP,appname)
+{
+  sh "scp /var/lib/jenkins/workspace/${Jobname}/webapp/target/webapp.war  unbuntu@${IP}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+def runSelenium(Jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${Jobname}/testing.jar"
+}
+def newDelivery(Jobname,IP,appname)
+{
+  sh "scp /var/lib/jenkins/workspace/${Jobname}/webapp/target/webapp.war  unbuntu@${IP}:/var/lib/tomcat9/webapps/${appname}.war"
+}
